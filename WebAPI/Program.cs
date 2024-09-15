2559ad870804064e51f6df98f33665715224df41
rakesh <rakeshrobin32153@gmail.com>
2024-09-15 16:55:03 +0530

    1. Move Json files int Json folder. 2,. Set Json fiile path into application

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -3,6 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
+builder.Configuration.AddJsonFile("Json/AppSettings.json");
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
